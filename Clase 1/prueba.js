036201f3c1d34e048fb3e75c536d9b8232ca1024
Francisco <franciscodemona@gmail.com>
2019-06-06 14:41:52 -0300

    Sumamos Clase 1

---
+++ Clase 1/prueba.js
@@ -0,0 +1,12 @@
+const inventario = [
+  {tipo:   "maquina", valor: 5000},
+  {tipo:   "maquina", valor:  650},
+  {tipo:      "silla", valor:   10},
+  {tipo: "sillon", valor: 1200},
+  {tipo:   "maquina", valor:   77}
+]
+
+// let valorTotalMaquina = TU_CÓDIGO_AQUÍ
+console.log('hola');
+console.log('como va');
+// console.log(valorTotalMaquina);
